5e3838101683a3dc2a3c58ef0c869557ccdc7b80
CauaGrigolatto <cauagrigolatto23@gmail.com>
2026-01-14 20:33:24 -0300

    Just refactor SCHEDULE FOR command
    I need to figure out a way to reduce useless and duplicated lines

--- serena/src/main/java/br/com/cauag/serena/core/syntax/ScheduleFor.java
+++ serena/src/main/java/br/com/cauag/serena/core/syntax/ScheduleFor.java
@@ -2,10 +2,16 @@ package br.com.cauag.serena.core.syntax;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import br.com.cauag.serena.core.Core;
 
 public class ScheduleFor extends ParameterReceiver {
+	
+	public ScheduleFor() {
+		super();
+	}
 	
 	@Override
 	protected boolean canExecute() {
@@ -14,23 +20,46 @@ public class ScheduleFor extends ParameterReceiver {
 
 	@Override
 	public int executeAndGetIndex(String complement, Core core) throws Exception {
+		if (complement == null || complement.isBlank()) throw new IllegalArgumentException("SCHEDULE statement is needed.");
+		
 		core.scheduleController.startSchedule(core.index + 1);
 		
-		String[] passedArgs = complement.split(",");
-		int n = passedArgs.length;
-		
-		String[] times = new String[n];
+		List<String> times = getTimes(complement);
+		int n = times.size();
 		
 		SimpleDateFormat formatter = new SimpleDateFormat(
 			core.configController.getConfig("DATE_TIME_FORMAT")
 		);
 		
 		for (int i = 0; i < n; i++) {
-			times[i] = applyParametersAndVariables(passedArgs[i].trim(), core);
-			Date date = formatter.parse(times[i]);
+			String time = times.get(i).trim();
+			time = applyParametersAndVariables(time, core);
+			Date date = formatter.parse(time);
 			core.scheduleController.addDate(date);
 		}
 		
 		return core.index;
+	}
+	
+	private List<String> getTimes(String str) {				
+		List<String> times = new LinkedList<String>();
+		StringBuilder sb = new StringBuilder();
+		int i = 0;
+				
+		for (boolean canAppend = false; i < str.length(); i++) {
+			if (str.charAt(i) == '"') {
+				canAppend = !canAppend;
+				
+				if (! sb.isEmpty()) {
+					times.add(sb.toString());
+					sb.setLength(0);
+				}
+			}
+			else if (canAppend) {
+				sb.append( str.charAt(i) );
+			}
+		}
+		
+		return times;
 	}
 }
