0dcecb2ff5003b63602828d3d16d199427a9b055
CauaGrigolatto <cauagrigolatto23@gmail.com>
2026-01-18 18:08:33 -0300

    Optimize how the parameters are set.

--- serena/src/main/java/br/com/cauag/serena/syntax/ExecutableAndParametersReceiver.java
+++ serena/src/main/java/br/com/cauag/serena/syntax/ExecutableAndParametersReceiver.java
@@ -1,43 +1,55 @@
 package br.com.cauag.serena.syntax;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import br.com.cauag.serena.core.Core;
 
 public abstract class ExecutableAndParametersReceiver extends Executable {
+	
+	private final Pattern variablePattern = Pattern.compile("\\$[a-z][a-zA-Z0-9_]*", Pattern.CASE_INSENSITIVE);
 
-	protected String applyParametersAndVariables(String token, Core core) {		
+	protected String applyParametersAndVariables(String token, Core core) {
 		if (token != null && ! token.isBlank()) {
-			Map<String, String> currentArgs = core.indexController.currentArgs();
-			
 			token = token.replaceAll("\\\\", "\\\\\\\\");
 			
-			if (currentArgs != null) {
-				for (Map.Entry<String, String> currArg : currentArgs.entrySet()) {
-					String argKey = currArg.getKey();
-					String argValue = currArg.getValue();
-					token = token.replaceAll("\\$" + argKey, argValue);
-				}
-			}
-			
-			Map<String, String> currentVariables = core.variablesController.variables();
-			
-			if (currentVariables != null) {
-				for (Map.Entry<String, String> variable : currentVariables.entrySet()) {
-					String varName = variable.getKey();
-					String varValue = variable.getValue();
-					token = token.replaceAll("\\$" + varName, varValue);
-				}
-			}
+			List<String> patterns = findPatterns(token);
 			
 			Map<String, String> forEachVariables = core.forEachController.variables();
+			token = apply(token, patterns, forEachVariables);
+
+			Map<String, String> blockArgs = core.indexController.currentArgs();
+			token = apply(token, patterns, blockArgs);
 			
-			if (forEachVariables != null) {
-				for (Map.Entry<String, String> variable : forEachVariables.entrySet()) {
-					String varName = variable.getKey();
-					String varValue = variable.getValue();
-					token = token.replaceAll("\\$" + varName, varValue);
+			Map<String, String> globalArgs = core.variablesController.variables();
+			token = apply(token, patterns, globalArgs);
+		}
+		
+		return token;
+	}
+	
+	protected String applyListParameters(String token, Core core) {
+		if (token != null && ! token.isBlank()) {
+			token = token.replaceAll("\\\\", "\\\\\\\\");
+			
+			List<String> patterns = findPatterns(token);
+			
+			Map<String, List<String>> lists = core.variablesController.lists();
+			
+			for (String pattern : patterns) {
+				List<String> list = lists.get(pattern.substring(1));
+				
+				if (list != null) {
+					StringBuilder sb = new StringBuilder();
+					
+					for (String el : list) {
+						sb.append("\"" + el + "\"");
+					}
+					
+					token = token.replace(pattern, sb.toString());
 				}
 			}
 		}
@@ -45,26 +57,28 @@ public abstract class ExecutableAndParametersReceiver extends Executable {
 		return token;
 	}
 	
-	protected String applyListParameters(String token, Core core) {
-		if (token != null && !token.isBlank()) {
-			Map<String, List<String>> lists = core.variablesController.lists();
-			
-			for (Map.Entry<String, List<String>> entry : lists.entrySet()) {
-				String varName = entry.getKey();
-				List<String> elements = entry.getValue();
+	private String apply(String token, List<String> patterns, Map<String, String> variableMap) {
+		if (variableMap != null && ! variableMap.isEmpty()) {			
+			for (String pattern : patterns) {
+				String value = variableMap.get(pattern.substring(1));
 				
-				StringBuilder quotedList = new StringBuilder();
-				
-				for (int i = 0; i < elements.size(); i++) {
-					String element = elements.get(i);
-					quotedList.append("\"" + element + "\"");
-					if (i < elements.size()-1) quotedList.append(" ");
+				if (value != null) {
+					token = token.replace(pattern, value);
 				}
-				
-				token = token.replaceAll("\\$" + varName, quotedList.toString());
 			}
 		}
 		
 		return token;
+	}
+	
+	private List<String> findPatterns(String token) {
+		Matcher matcher = variablePattern.matcher(token);
+		List<String> recognizedPatterns = new LinkedList<String>();
+		
+		while (matcher.find()) {
+			recognizedPatterns.add(matcher.group());
+		}
+		
+		return recognizedPatterns;
 	}
 }
